cb25a555ae9672c4fccc448a2f0ad353baf8ce4f
move style to head <timmortal44@yahoo.com>
2022-10-02 23:32:31 +0100

    add api url

--- js/index.js
+++ js/index.js
@@ -4,3 +4,4 @@ const search = document.getElementById('search')
 
 const APIKEY = "3265874a2c77ae4a04bb96236a642d2f";
 
+const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`
