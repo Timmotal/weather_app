6c29835badd3408420e9fd8d32583f61fa3c3183
move style to head <timmortal44@yahoo.com>
2022-10-02 23:32:20 +0100

    add API key

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@ const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search') 
 
-
+const APIKEY = "3265874a2c77ae4a04bb96236a642d2f";
 
