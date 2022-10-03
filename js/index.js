cf1c70b75e76d499c7bc656386234a1bffe10012
move style to head <timmortal44@yahoo.com>
2022-10-03 22:53:46 +0100

    set const to fetchurl

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ const APIKEY = "3265874a2c77ae4a04bb96236a642d2f";
 const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`
 
 async function getWeatherByLocation(city) {
-    
+    const resp = await fetch(url(city), {
      
    
     
